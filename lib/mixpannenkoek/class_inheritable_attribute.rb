53267e2736ab6e7076ef3fa6ab14467fdb9eb242
Derek Kraan <derek.kraan@gmail.com>
2014-05-28 12:09:21 +0000

    Minor refactor of ClassInheritableAttribute

--- lib/mixpannenkoek/class_inheritable_attribute.rb
+++ lib/mixpannenkoek/class_inheritable_attribute.rb
@@ -2,20 +2,28 @@ module Mixpannenkoek
   module ClassInheritableAttribute
     def class_inheritable_attribute(*attributes)
       attributes.map(&:to_sym).each do |attribute|
-        define_singleton_method("#{attribute}=") do |value|
-          @@class_inheritable_attributes ||= {}
-          @@class_inheritable_attributes[attribute] ||= {}
+        create_setter(attribute)
+        create_getter(attribute)
+      end
+    end
 
-          @@class_inheritable_attributes[attribute][self.name] = value
-        end
-        define_singleton_method(attribute) do
-          if @@class_inheritable_attributes[attribute] && @@class_inheritable_attributes[attribute].has_key?(self.name)
-            @@class_inheritable_attributes[attribute][self.name]
-          elsif superclass.respond_to?(attribute)
-            superclass.send(attribute)
-          else
-            nil
-          end
+    def create_setter(attribute)
+      define_singleton_method("#{attribute}=") do |value|
+        @@class_inheritable_attributes ||= {}
+        @@class_inheritable_attributes[attribute] ||= {}
+
+        @@class_inheritable_attributes[attribute][self.name] = value
+      end
+    end
+
+    def create_getter(attribute)
+      define_singleton_method(attribute) do
+        if @@class_inheritable_attributes[attribute] && @@class_inheritable_attributes[attribute].has_key?(self.name)
+          @@class_inheritable_attributes[attribute][self.name]
+        elsif superclass.respond_to?(attribute)
+          superclass.send(attribute)
+        else
+          nil
         end
       end
     end
